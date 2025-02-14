e2a44ff0c4124e833616ca527266fe560ba21f10
Matheus <matheudbaf0123@gmail.com>
2025-02-14 13:15:47 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,44 @@
+# React Magical Carousel
+
+A infinite carousel component for React
+
+## Installation
+
+```
+npm install react-magical-carousel
+```
+
+## Example
+
+```tsx
+import ReactMagicalCarousel from 'react-magical-carousel'
+import leftIcon from './assets/icons/arrow-left.png'
+import rightIcon from './assets/icons/arrow-right.png'
+
+<ReactMagicalCarousel
+slideWidth={'100%'}
+ratio={9/16}
+renderJumpBtns={true}
+previousBtn={(onJump) => <img className='previous-btn' src={leftIcon} alt='previous-btn' onClick={onJump} />}
+nextBtn={(onJump) => <img className='next-btn' src={leftIcon} alt='next-btn' onClick={onJump} />}
+onSelectSlide={({ currentSlide }) => console.log(currentSlide)}>
+    {images.map((x, i) => <img key={'img_'+i} src={x} alt={'img_'+i} />)}
+</ReactMagicalCarousel>
+```
+
+## Props
+
+| Name         | Type      | Default | Description
+| ------------- |:---------:| :-----:|-------------
+| `children` | `JSX.Element | JSX.Element[]` | `The components for the slides, if it's length is 1, the carousel features will be disabled`
+| `slideWidth` | `string` | `100%` | `The width of the slides, it can be in 'px' or '%'. Not the width of the wrapper component, it's width is 100% of it's parent`
+| `slideHeight` | `string` | `500px` | `The fixed height of slides, it can be in 'px' or 'vh', it overwrites the 'ratio' prop`
+| `ratio` | `number` | `-` | `The ratio proportion of the slides, it will force the same proportion in all screen sizes`
+| `slideMargin` | `string` | `-` | `The margin between each slide, it can be in 'px' or '%'`
+| `renderDotBtns` | `boolean` | `false` | `It tells if the component should render the bottom navigation`
+| `renderJumpBtns` | `boolean` | `false` | `It tells if the component should render the arrow buttons on each side`
+| `nextBtn` | `(onJump: () => Promise<void>) => JSX.Element` | `-` | `It overwrites the default next slide arrow button`
+| `previousBtn` | `(onJump: () => Promise<void>) => JSX.Element` | `-` | `It overwrites the default previous slide arrow button`
+| `jumpInterval` | `number` | `-` | `The time in miliseconds in which the carousel will wait to  automatically pass between it's slides, it won't happen if the prop it's not set`
+| `transition` | `number` | `250` | `The time in miliseconds which takes for a slide jump to another`
+| `onSelectSlide` | `Function` | `-` | `It returns an object with the current slide index and the jump count of slides from the last one to the current one`
---
+++ webpack.config.js
@@ -0,0 +1,71 @@
+// webpack.config.js
+// Import dependencies
+const path = require('path');
+// Takes our css/scss files and compiles them into one single css file
+const MiniCssExtractPlugin  = require('mini-css-extract-plugin');
+// Using index.html as a template puts an html file in our build folder
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+// Define the config object
+const config = {
+    target: 'web',
+    entry: './src/index.tsx', // Our entry file, will take all files connected to index.js and compile them together
+    output: {
+            path: path.resolve(__dirname,'dist'), // Tells webpack where to put the output files
+            filename: 'index.js', // Naming our compiled ES5 file
+            library: 'react-magical-carousel',
+            libraryTarget: 'umd',
+            globalObject: 'this',
+            umdNamedDefine: true
+        },
+    module: {
+        rules: [
+            // Tells webpack to use Babel to compile .js files
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                  // Creates `style` nodes from JS strings
+                  "style-loader",
+                  // Translates CSS into CommonJS
+                  "css-loader",
+                  // Compiles Sass to CSS
+                  "sass-loader",
+                ],
+            },
+            // Tells webpack to use MiniCss to compile css/scss files. If that fails, use css-loader then sass-loader
+            // {
+            //     test: /\.sass$/,
+            //     use: [
+            //     MiniCssExtractPlugin.loader,
+            //         "css-loader",
+            //         "sass-loader"
+            //     ]
+            // },
+            // Converts images into base64
+            {
+                test: /\.(png|jpg|gif|svg)$/i,
+                use: [
+                    {
+                        loader: 'url-loader',
+                        options: {
+                        limit: true,
+                        },
+                    },
+                ],
+            },
+        ]
+    },
+    plugins: [
+        // new HtmlWebPackPlugin({
+        //   template: "./index.html", // Template for the html file used in build
+        //   filename: "./index.html" // Name of the html file webpack will put in the build folder
+        // }),
+        new MiniCssExtractPlugin({filename: 'style.css'}) // Name of our CSS file in the build folder
+    ]
+};
+// export the config object so when webpack runs it knows to use it.
+module.exports = config;
